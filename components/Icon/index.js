8e4dd56b2a26cb60c48085175fc961175818fc78
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2022-10-07 03:25:27 -0500

    added icon component

---
+++ components/Icon/index.js
@@ -0,0 +1,15 @@
+export default function Icon({width,imgRoute}){
+    return(
+        <>  
+                <img src={imgRoute}>
+                </img>   
+                <style jsx>{`
+                    img {
+                        width:${width};
+                        margin-right: 10px;
+                        
+                    }
+                `}</style>
+        </>
+    )
+}
