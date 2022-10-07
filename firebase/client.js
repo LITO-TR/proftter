d3465441ba4523c59358f38e38175877de64056a
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2022-10-07 03:26:56 -0500

    added firebase login

---
+++ firebase/client.js
@@ -0,0 +1,42 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC2uMqdR_5L4_ha5uRjwnmvVhlSggjheaY",
+    authDomain: "proftter.firebaseapp.com",
+    projectId: "proftter",
+    storageBucket: "proftter.appspot.com",
+    messagingSenderId: "959735755221",
+    appId: "1:959735755221:web:1501ff646d8e04b6144d5c",
+    measurementId: "G-921PTR6HWD"
+  }
+
+!firebase.apps.length &&firebase.initializeApp
+(firebaseConfig)
+
+const mapUserFromFirebaseAuthToUser = (user) =>{
+    const {displayName, email, photoURL} = user
+    
+    return {
+        avatar: photoURL,
+        username: displayName,
+        email
+    }
+}
+export const onAuthStateChanged = (onChange) =>{
+    return firebase
+    .auth()
+    .onAuthStateChanged(user =>{
+        const normalizeUser = 
+            mapUserFromFirebaseAuthToUser(user)
+            onChange(normalizeUser)
+    })
+}
+export const loginWithGoogle = () => {
+    const googleProvider = new firebase.auth.GoogleAuthProvider();
+    return firebase.auth()
+    .signInWithPopup(googleProvider)
+    .then(mapUserFromFirebaseAuthToUser)
+}
+
--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,27 @@ import Head from 'next/head'
 import Link from 'next/link'
 import {useRouter} from 'next/router'
 import AppLayout from '../components/AppLayout'
+import Button from '../components/Button'
+import Icon from '../components/Icon' 
+import {colors} from '../styles/theme'
+import { loginWithGoogle, onAuthStateChanged } from '../firebase/client'
+import { useEffect, useState } from 'react'
 // devit
 
 export default function Home() {
+  const [user, setUser] = useState(null)
+  useEffect(()=> {
+    onAuthStateChanged(setUser)
+  },[])
+  const handleClick = () => {
+    loginWithGoogle()
+    .then(user => {
+      const { avatar, username, url} = user 
+      setUser(user)
+    }).catch(err => {
+      console.log(err)
+    })
+  }
   const router = useRouter()
 
   return (
@@ -15,35 +33,42 @@ export default function Home() {
       </Head>
 
       <AppLayout>
-        <h1>
-          <a href="https://nextjs.org">devter</a>
-        </h1>
-        <nav>
-          <Link href='/timeline'>
-            <a>
-              timeline
-            </a>
-          </Link>
-        </nav>
+        <section>
+        <div>
+        <Icon imgRoute='/UGEL_LOGO.png' width='120px' alt='Logo'></Icon>
+        </div>
+        <div>
+        
+          {
+            user ===null ?
+            <Button onClick={handleClick}>
+              <Icon width="17px"  imgRoute="/icon-google.png"></Icon>
+              Login With Gmail
+            </Button>
+            : <div>
+                <img src={user.avatar}></img>
+                <strong>{user.username}</strong>
+              </div>
+          }
+          
+        </div>
+        </section>
       </AppLayout>
 
       <style jsx>{`
-        h1 {
-          text-align: center;
-          font-size: 48px;
-        }
-        nav {
-          font-size: 24px;
-          text-align: center;
-        }
-        .another-title {
-          color: #333;
-          font-size: 24px;
-        }
-        a {
-          color: orange;
-          text-decoration: none;
-        }
+          div {
+            margin: 8px
+          }
+          section {
+            display:grid;
+            height: 100%;
+            place-content: center;
+            place-items: center;
+          }
+          h2 {
+            font-size: 14px;
+            color: ${colors.primary}
+          }
       `}</style>
     </>
   )
