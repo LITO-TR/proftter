6ced0dc81964ffbe28ea9fa704af96bad463b66b
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2022-10-07 03:26:22 -0500

    added button component

---
+++ components/Button/index.js
@@ -0,0 +1,28 @@
+import { colors } from "../../styles/theme";
+
+export default function Button({children, onClick}){
+    return(
+        <>
+        <button onClick={onClick}>
+            {children}
+        </button>
+        <style jsx>{`
+                button {
+                    background: ${colors.black};
+                    border: 0;
+                    color: #fff;
+                    border-radius: 9999px;
+                    font-size: 16px;
+                    font-weight: 800;
+                    padding: 8px 24px;
+                    transiton: opacity .33s ease;
+
+                }
+                button:hover {
+                    opacity: .7;
+                }
+            `}
+        </style>
+        </>
+    )
+}
